78fa29e23e2ba173d6ca2a990a6a25c65ebb774d
Lesia Grapeniuk <lesiar29@ukr.net>
2025-01-27 19:18:37 +0200

    Removed unnecessary comments from the code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,23 +3,19 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
-// import LaraLightBlue from '@primeng/themes/lara-light-blue'; // Використання теми lara-light-blue
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    // Поточні налаштування Angular
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
 
-    // Додавання анімацій (необхідно для PrimeNG)
     provideAnimations(),
 
-    // Конфігурація PrimeNG із підключенням теми
     providePrimeNG({
       theme: {
-        preset: Aura, // Використання теми Lara Light Blue
+        preset: Aura,
       },
     }),
   ],
