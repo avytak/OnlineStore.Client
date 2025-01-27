a9ad9afdd2f77eeaf307c38b9dbaf4d99512d9e7
Lesia Grapeniuk <lesiar29@ukr.net>
2025-01-27 19:10:13 +0200

    add primeNg

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+// import LaraLightBlue from '@primeng/themes/lara-light-blue'; // Використання теми lara-light-blue
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    // Поточні налаштування Angular
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    // Додавання анімацій (необхідно для PrimeNG)
+    provideAnimations(),
+
+    // Конфігурація PrimeNG із підключенням теми
+    providePrimeNG({
+      theme: {
+        preset: Aura, // Використання теми Lara Light Blue
+      },
+    }),
+  ],
 };
